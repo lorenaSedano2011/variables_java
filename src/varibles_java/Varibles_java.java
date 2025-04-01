bf922e718e17040dd601c665e7627457bce23456
lorenasedano2011 <nicolva2011@gmail.com>
2025-03-31 20:24:17 -0500

    version4

---
+++ src/varibles_java/Varibles_java.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package varibles_java;
+
+/**
+ *
+ * @author pc-linx
+ */
+public class Varibles_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "lorena"; // variable de tipo texto
+        int numero = 1; // las variables de tipo int son numeros enteros y no llevan comillas
+        Double precio = 1.50;//las variables de tipo Double son decimales
+        Boolean stop = true;//variables boolean verdadero o falso
+        
+    }
+    
+}
